1b441126e1afa3ffcc3b67dfe6a52f5104da171e
slavok <badulyakyaroslav@gmail.com>
2022-10-18 23:18:54 +0300

    v.11

--- script.js
+++ script.js
@@ -1,12 +1,44 @@
-const btn = document.querySelector(".btn");
-const content = document.querySelector(".content");
-btn.addEventListener("click", Click);
-function Click() {
-  console.log(content.classList);
-  if (content.classList.contains("hidden")) {
-    btn.textContent = "Hide text";
-  } else {
-    btn.textContent = "Show text";
-  }
-  content.classList.toggle("hidden");
-}
+(() => {
+  
+  const todo = document.getElementById('todo');
+  const inputFirst =document.getElementById('inputFirst');
+  const inputSecond =document.getElementById('inputSecond');
+  const button = document.getElementById('btn');
+  
+  let i = 0;
+
+  button.addEventListener('click', () => {    
+    const divBox = document.createElement('div'); 
+
+    const newSpan = document.createElement('span');
+    newSpan.innerText = 'ToDo' + ++i;
+    todo.append(newSpan);
+    console.log(newSpan);
+
+    const title = document.createElement('h2');
+    title.innerText = inputFirst.value;  
+    
+    const description = document.createElement('p');
+    description.innerText = inputSecond.value; 
+    
+    const del = document.createElement('button');
+    del.innerText ='delete';
+    
+    del.addEventListener('click', () => {
+      divBox.remove()
+    }); 
+
+    const check = document.createElement('input');
+    check.setAttribute('type', 'checkbox');
+
+    console.dir(inputFirst.value);
+    console.dir(inputSecond.value);    
+    divBox.append(newSpan, title, description, check, del);
+    todo.append(divBox);
+
+    inputFirst.value ='';
+    inputSecond.value ='';
+  });
+})();
+
+
